f8a7f7912b99dbddae3986ecde41fc1c4cea5a61
Hoanghahiha <81789681+Hoanghahiha@users.noreply.github.com>
2022-03-22 09:41:33 +0700

    cn

--- Assigment2/Fraction.java
+++ Assigment2/Fraction.java
@@ -1,27 +1,76 @@
 package Assigment2;
 
 import java.util.Scanner;
-
 public class Fraction {
-    int tuso;
-    int mauso;
+    int tuso,mauso;
+    Scanner input=new Scanner(System.in);
 
-    public int getTuso() {
-        return tuso;
+    public void inputData() {
+        System.out.print("Nhap tu so: ");
+        this.tuso=input.nextInt();
+        do
+        {
+            System.out.print("Nhap mau so: ");
+            this.mauso=input.nextInt();
+        }
+        while (this.mauso==0);
     }
 
-    public void setTuso(int tuso) {
-        this.tuso = tuso;
+    public void outputData(String y) {
+        System.out.println(y+" " +this.tuso+"/"+this.mauso);
+    }
+    public int UCLN(int a,int b) {
+        if ((a==0) || (b==0))
+            return a+b;
+        while(a!=b)
+        {
+            if(a>b)
+                a=a-b;
+            else
+                b=b-a;
+        }
+        return a;
+    }
+    public void toiGian() {
+        int i=UCLN(tuso,mauso);
+        this.tuso = this.tuso/i;
+        this.mauso = this.mauso/i;
     }
 
-    public Fraction(){
-        Scanner fc = new Scanner(System.in);
-        tuso = fc.nextInt();
-        mauso = fc.nextInt();
-        System.out.println("Phan so vua nhap la: " + (float)tuso/mauso);
-        System.out.println("Phan so rut gon la: " );
-        System.out.println("Phan  so nghich dao la: " + mauso/tuso);
+    public void ngichdaophanso() {
+        int tg=this.tuso;
+        this.tuso = this.mauso;
+        this.mauso = tg;
     }
 
+    public Fraction tong(Fraction f) {
+        Fraction ps=new Fraction();
+        ps.tuso = ((this.tuso*f.mauso) + (this.mauso*f.tuso));
+        ps.mauso = (this.mauso*f.mauso);
+        ps.toiGian();
+        return ps;
+    }
 
+    public Fraction hieu(Fraction f) {
+        Fraction ps=new Fraction();
+        ps.tuso = (this.tuso*f.mauso) - (this.mauso*f.tuso);
+        ps.mauso = (this.mauso*f.mauso);
+        return ps;
+    }
+
+    public Fraction tich(Fraction f) {
+        Fraction ps=new Fraction();
+        ps.tuso = this.tuso*f.tuso;
+        ps.mauso = this.mauso*f.mauso;
+        ps.toiGian();
+        return ps;
+    }
+
+    public Fraction thuong(Fraction f) {
+        Fraction ps=new Fraction();
+        ps.tuso = this.tuso*f.mauso;
+        ps.mauso = this.mauso*f.tuso;
+        ps.toiGian();
+        return ps;
+    }
 }
--- Assigment2/Main.java
+++ Assigment2/Main.java
@@ -2,6 +2,19 @@ package Assigment2;
 
 public class Main {
     public static void main(String args[]){
-        Fraction a = new Fraction();
+        Fraction ps1 = new Fraction();
+        Fraction ps2 = new Fraction();
+        ps1.inputData();
+        ps2.inputData();
+        ps1.outputData("Phan so 1 la: ");
+        ps2.outputData("Phan so 2 la: ");
+        ps1.toiGian();
+        ps1.outputData("Phan so 1 sau khi toi gian la: ");
+        ps2.toiGian();
+        ps2.outputData("Phan so 2 sau khi toi gian la: ");
+        ps1.tong(ps2).outputData("Tong hai phan so la: ");
+        ps1.hieu(ps2).outputData("Hieu hai phan so la: ");
+        ps1.tich(ps2).outputData("Tich hai phan so la: ");
+        ps1.thuong(ps2).outputData("Thuong hai phan so la:");
     }
 }
